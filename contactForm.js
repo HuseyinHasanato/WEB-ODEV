9ccfb73a3d021735e484757a33721c336131c17d
HÜSEYİN HASANATO <104093869+HuseyinHasanato@users.noreply.github.com>
2023-05-17 03:41:50 -0700

    sadsad

--- contactForm.js
+++ contactForm.js
@@ -1,13 +1,11 @@
 document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // يمنع إعادة تحميل الصفحة بعد الضغط على الزر
+    event.preventDefault(); 
   
-    // استخراج قيم المدخلات
     var name = document.getElementById("name").value;
     var lastName = document.getElementById("lastName").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
   
-    // بناء بيانات الرسالة
     var data = {
       name: name,
       lastName: lastName,
@@ -15,7 +13,6 @@ document.getElementById("contactForm").addEventListener("submit", function(event
       message: message
     };
   
-    // إرسال البيانات إلى الخادم
     fetch("/sendEmail", {
       method: "POST",
       headers: {
@@ -27,7 +24,6 @@ document.getElementById("contactForm").addEventListener("submit", function(event
         return response.json();
       })
       .then(function(data) {
-        // عرض رسالة الاستجابة من الخادم
         alert(data.message);
       })
       .catch(function(error) {
