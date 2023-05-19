00835654eac3735666c0dfc156e6f2de823ac800
HÜSEYİN HASANATO <104093869+HuseyinHasanato@users.noreply.github.com>
2023-05-19 10:32:52 -0700

    Create error.php

---
+++ login_logic/error.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>yanlis</title>
+</head>
+<body>
+    <p>girdiginiz sifre yada email yanllistir</p>
+    <a href="index.php">tekrar dene</a>
+</body>
+</html>
